c2d9672a76add560d09856abe99dff485250ac3b
Kurtis <kurtis@edge.app>
2022-05-05 12:11:29 -0700

    Add testHooks to WalletList to return row IDs

--- src/components/themed/WalletList.js
+++ src/components/themed/WalletList.js
@@ -1,7 +1,8 @@
 // @flow
 
+import { useCavy, wrap } from 'cavy'
 import * as React from 'react'
-import { FlatList, SectionList } from 'react-native'
+import { SectionList } from 'react-native'
 
 import { selectWallet } from '../../actions/WalletActions.js'
 import { useAllTokens } from '../../hooks/useAllTokens.js'
@@ -10,6 +11,7 @@ import s from '../../locales/strings'
 import { getExchangeDenominationFromState } from '../../selectors/DenominationSelectors.js'
 import { calculateFiatBalance } from '../../selectors/WalletSelectors.js'
 import { useMemo } from '../../types/reactHooks.js'
+import { FlatList } from '../../types/reactNative.js'
 import { useDispatch, useSelector } from '../../types/reactRedux.js'
 import type { CreateTokenType, CreateWalletType, FlatListItem, GuiWallet } from '../../types/types.js'
 import { type EdgeTokenId, asSafeDefaultGuiWallet } from '../../types/types.js'
@@ -59,7 +61,7 @@ type Props = {|
   onPress?: (walletId: string, currencyCode: string) => void
 |}
 
-export function WalletList(props: Props) {
+export function WalletListComponent(props: Props) {
   const dispatch = useDispatch()
   const {
     // Filtering:
@@ -79,6 +81,7 @@ export function WalletList(props: Props) {
   } = props
 
   const theme = useTheme()
+  const generateTestHook = useCavy()
   const margin = sidesToMargin(mapSides(fixSides(marginRem, 0), theme.rem))
 
   const handlePress = useMemo(
@@ -347,8 +350,10 @@ export function WalletList(props: Props) {
     )
   }
 
-  return <FlatList data={walletList} keyboardShouldPersistTaps="handled" renderItem={renderRow} style={margin} />
+  return <FlatList data={walletList} keyboardShouldPersistTaps="handled" renderItem={renderRow} style={margin} ref={generateTestHook('WalletList.WalletId')} />
 }
+
+export const WalletList = wrap(WalletListComponent)
 
 type FilterDetailsType = {
   // For searching:
