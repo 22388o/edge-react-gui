abe8c4c84179f9168e06afa7945238dd9b1e94f9
Jon Tzeng <jon@edge.app>
2022-05-09 21:39:37 -0700

    Implement useFiatText hook
    useFiatText contains common props needed for new text components.

--- src/hooks/useFiatText.js
+++ src/hooks/useFiatText.js
@@ -8,27 +8,36 @@ import { DECIMAL_PRECISION, formatFiatString, zeroString } from '../util/utils'
 
 const defaultMultiplier = Math.pow(10, DECIMAL_PRECISION).toString()
 type Props = {
+  appendFiatCurrencyCode?: boolean,
+  autoPrecision?: boolean,
   cryptoCurrencyCode: string,
   cryptoExchangeMultiplier?: string,
-  nativeCryptoAmount?: string,
-  appendFiatCurrencyCode?: boolean,
   fiatSymbolSpace?: boolean,
   isoFiatCurrencyCode?: string,
+<<<<<<< HEAD
   parenthesisEnclosed?: boolean,
   autoPrecision?: boolean,
+=======
+  nativeCryptoAmount?: string,
+>>>>>>> 718477c... Update FiatText API, Integrate into new TickerText Component
   noGrouping?: boolean
 }
 
 export const useFiatText = (props: Props) => {
   const {
-    cryptoExchangeMultiplier = defaultMultiplier,
     appendFiatCurrencyCode,
+<<<<<<< HEAD
     nativeCryptoAmount = cryptoExchangeMultiplier,
     fiatSymbolSpace,
     parenthesisEnclosed,
-    cryptoCurrencyCode,
-    isoFiatCurrencyCode = USD_FIAT,
+=======
     autoPrecision,
+>>>>>>> 718477c... Update FiatText API, Integrate into new TickerText Component
+    cryptoCurrencyCode,
+    cryptoExchangeMultiplier = defaultMultiplier,
+    fiatSymbolSpace,
+    isoFiatCurrencyCode = USD_FIAT,
+    nativeCryptoAmount = cryptoExchangeMultiplier,
     noGrouping = false
   } = props
 
@@ -49,6 +58,7 @@ export const useFiatText = (props: Props) => {
         })
       : '0'
 
+<<<<<<< HEAD
   // Create FiatText' prefix
   const fiatSymbol = getSymbolFromCurrency(isoFiatCurrencyCode)
   const fiatSymbolFmt = fiatSymbolSpace ? `${fiatSymbol} ` : fiatSymbol
@@ -59,4 +69,9 @@ export const useFiatText = (props: Props) => {
 
   const fiatText = `${prefix}${fiatString}${fiatCurrencyCode}${suffix}`
   return { fiatAmount, fiatString, fiatText }
+=======
+  const fiatSymbol = `${getSymbolFromCurrency(isoFiatCurrencyCode)}${fiatSymbolSpace ? ' ' : ''}`
+  const fiatCurrencyCode = appendFiatCurrencyCode ? ` ${isoFiatCurrencyCode.replace('iso:', '')}` : ''
+  return `${fiatSymbol}${fiatString}${fiatCurrencyCode}`
+>>>>>>> 718477c... Update FiatText API, Integrate into new TickerText Component
 }
